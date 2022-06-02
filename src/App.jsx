cb5167a74a99af7c28576ceb7f88f3b334d737ee
ShahriyarHussain <shossainshihab36@gmail.com>
2022-06-02 23:23:58 +0600

    Change button condition :construction:

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [msg, setMsg] = useState("Yes");
   const [condition, setCondition] = useState(true);
 
-  const btnClick = () => {
-    if (condition) {
-      setMsg("No");
-    } else {
-      setMsg("Yes");
-    }
+  function flipCondition() {
     setCondition(!condition);
-  };
+  }
 
   return (
     <div>
       <button
-        className='bg-blue-600 text-white font-bold rounded-md hover:bg-blue-800 transition-all p-2 ml-3 mt-3'
-        onClick={btnClick}>
-        {msg}
+        className="bg-blue-600 text-white font-bold rounded-md hover:bg-blue-800 transition-all p-2 ml-3 mt-3"
+        onClick={flipCondition}
+      >
+        {condition ? "Yes" : "No"}
       </button>
       {/* <p>{demo}</p> */}
     </div>
